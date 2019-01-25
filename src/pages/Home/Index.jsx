61a831d9aa47a4d246e0d6538464057ffc276d19
edandaniel <edandaniel@gmail.com>
2019-01-24 23:18:07 -0200

    add input for api search

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
 
 class Home extends React.Component {
+    constructor(){
+        super();
+        this.search = this.search.bind(this);
+    }
+
     render(){
-        return (<div>Home</div>);
+        return (
+        <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+        <input onChange={this.search} className="mdl-textfield__input" type="text" id="search" />
+        <label className="mdl-textfield__label" htmlFor="search">Digite para buscar...</label>
+        </div>
+        );
     };
+    
+
+    search(event){
+        console.log(event.currentTarget.value);
+    }
 }
 
 export default Home;
